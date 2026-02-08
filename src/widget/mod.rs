9f64aa7aa2651c0e18befae73dfbedab64456219
Alessio Deiana <adeiana@gmail.com>
2026-02-08 16:02:11 -0600

    Recognize DropdownButton as a Button widget type

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -49,7 +49,7 @@ impl WidgetType {
         let lower = s.to_ascii_lowercase();
         match lower.as_str() {
             "frame" => Some(Self::Frame),
-            "button" | "itembutton" | "containedalertframe" => Some(Self::Button),
+            "button" | "dropdownbutton" | "itembutton" | "containedalertframe" => Some(Self::Button),
             "fontstring" => Some(Self::FontString),
             "texture" => Some(Self::Texture),
             "editbox" => Some(Self::EditBox),
