e147af0e50a18e6a71fca0b1055256b3f78d18aa
Alessio Deiana <adeiana@gmail.com>
2026-09-04 21:42:19 -0500

    Gate timed signal app data export

--- src/lua_api/mod.rs
+++ src/lua_api/mod.rs
@@ -51,6 +51,7 @@ pub mod workarounds;
 pub(crate) mod workarounds_editmode;
 
 // Re-export public types
+#[cfg(feature = "retail-12-1-5")]
 pub(crate) use env::WowLuaAppData;
 pub use env::WowLuaEnv;
 pub use globals::global_frames::hide_runtime_hidden_frames;
