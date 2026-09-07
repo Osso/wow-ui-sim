16362d6069a441a8db25ebe2980da45abe81c3f8
Alessio Deiana <adeiana@gmail.com>
2026-09-07 16:48:37 -0500

    Retain native frame identity in forbidden object tables

--- src/lua_api/env_init/shared_bootstrap.lua
+++ src/lua_api/env_init/shared_bootstrap.lua
@@ -135,7 +135,7 @@ function GetForbiddenObjectTable(object)
   end
   local forbidden = __wow_forbidden_object_tables[object]
   if forbidden == nil then
-    forbidden = setmetatable({ __wowPublicObject = object }, {
+    forbidden = setmetatable({ [0] = rawget(object, 0), __wowPublicObject = object }, {
       __index = function(_, key)
         local method = object[key]
         if type(method) ~= "function" then
