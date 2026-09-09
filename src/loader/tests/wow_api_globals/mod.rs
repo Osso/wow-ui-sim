17c085065aaf47386d5309c1ef82287c2d4e1a30
Alessio Deiana <adeiana@gmail.com>
2026-09-09 01:27:31 -0500

    Format WeatherType test module registration

--- src/loader/tests/wow_api_globals/mod.rs
+++ src/loader/tests/wow_api_globals/mod.rs
@@ -23,12 +23,12 @@ mod patch_12_1_5_aura_application_options;
 mod patch_12_1_5_aura_caster_name_options;
 mod patch_12_1_5_bag_forbidden_enums;
 mod patch_12_1_5_bonus_stat_index_enums;
-mod patch_12_1_5_weather_type_enums;
 mod patch_12_1_5_cooldown_thresholds;
 #[cfg(any(feature = "client-ptr", feature = "client-retail"))]
 mod patch_12_1_5_fragment_id_enums;
 mod patch_12_1_5_table_count_info;
 mod patch_12_1_5_table_util_removal;
+mod patch_12_1_5_weather_type_enums;
 
 macro_rules! active_retail_12_0_0_test_modules {
     ($($module:item)*) => {
