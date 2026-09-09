bbccf172e0abcd016bb53c1e06ae66da88a33c6a
Alessio Deiana <adeiana@gmail.com>
2026-09-09 02:14:42 -0500

    Sort locale enum test module registration

--- src/loader/tests/wow_api_globals/mod.rs
+++ src/loader/tests/wow_api_globals/mod.rs
@@ -23,11 +23,11 @@ mod patch_12_1_5_aura_application_options;
 mod patch_12_1_5_aura_caster_name_options;
 mod patch_12_1_5_bag_forbidden_enums;
 mod patch_12_1_5_bonus_stat_index_enums;
-#[cfg(any(feature = "client-ptr", feature = "client-retail"))]
-mod patch_12_1_5_intl_enums;
 mod patch_12_1_5_cooldown_thresholds;
 #[cfg(any(feature = "client-ptr", feature = "client-retail"))]
 mod patch_12_1_5_fragment_id_enums;
+#[cfg(any(feature = "client-ptr", feature = "client-retail"))]
+mod patch_12_1_5_intl_enums;
 mod patch_12_1_5_player_data_flag_enums;
 mod patch_12_1_5_table_count_info;
 mod patch_12_1_5_table_util_removal;
